bf5e1f011819bd8c170f45044ba3661799dbcf8e
Fernando Rodrigo <ferrodbi@gmail.com>
2017-12-14 15:38:14 +0100

    Getting the detail fragment into the weather_detail_container

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -19,40 +19,46 @@ package com.example.android.sunshine.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 
 //import android.widget.ShareActionProvider;
 
 public class DetailActivity extends ActionBarActivity {
     private static final String LOG_TAG = DetailActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         if (savedInstanceState == null) {
+            Bundle arguments = new Bundle();
+            arguments.putParcelable(DetailFragment.DETAIL_URI, getIntent().getData());
+            DetailFragment fragment = new DetailFragment();
+            fragment.setArguments(arguments);
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new DetailFragment())
+                    .add(R.id.weather_detail_container, fragment)
                     .commit();
         }
     }
 
 
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.detailfragment, menu);
-        MenuItem menuItem = menu.findItem(R.id.action_share);
-        android.support.v7.widget.ShareActionProvider mShareActionProvider =
-                (android.support.v7.widget.ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
-        if (mShareActionProvider != null) {
-            mShareActionProvider.setShareIntent(
-                    createShareForecastIntent());
-
-        } else {
-            Log.d(LOG_TAG, "Share action provider is null");
-        }
-        return true;
-    }*/
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.detailfragment, menu);
+//        MenuItem menuItem = menu.findItem(R.id.action_share);
+//        android.support.v7.widget.ShareActionProvider mShareActionProvider =
+//                (android.support.v7.widget.ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+//        if (mShareActionProvider != null) {
+//            mShareActionProvider.setShareIntent(
+//                    createShareForecastIntent());
+//
+//        } else {
+//            Log.d(LOG_TAG, "Share action provider is null");
+//        }
+//        return true;
+//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
